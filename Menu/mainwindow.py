88d083337c3f20fb8fe2bf2b9563bbbe9a29ef22
TsenkoTsenkov <tseni.tsenkov@gmail.com>
2020-05-02 22:31:41 +0300

    Added switching between layers

--- Menu/mainwindow.py
+++ Menu/mainwindow.py
@@ -1,38 +1,89 @@
 import sys
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout, QLabel, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QFont 
+import pygame
 
+'''
 credits = 0
+sampleMusic = 'TortureDance.mp3'
 
-class MainMenu(QWidget):
+def playMusic(self):
+
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(sampleMusic)
+    pygame.mixer.music.play()
+'''
+
+class MainWindow(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal()
+
     def __init__(self):
-        super().__init__()
-        
-        self.initUI()
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Main Window')
 
-    def initUI(self):
-        grid = QGridLayout()
-        
-        self.text = "Credits: {0}".format(credits)
-        self.label = QLabel(self.text, self)
-        grid.addWidget(self.label, 0, 0, Qt.AlignBottom)
+        layout = QtWidgets.QGridLayout()
 
-        mbtn = QPushButton('Music', self)
-        mbtn.resize(mbtn.sizeHint())
-        mbtn.move(120, 40)
+        self.button = QtWidgets.QPushButton('Main')
+        self.button.clicked.connect(self.switch)
+        layout.addWidget(self.button)
 
-        qbtn = QPushButton('Quit', self)
-        qbtn.clicked.connect(QApplication.instance().quit)
-        qbtn.resize(qbtn.sizeHint())
-        qbtn.move(120, 100)
+        self.setLayout(layout)
 
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('JukeBox')    
-        self.show()
+    def switch(self):
+        self.switch_window.emit()
+
+
+class MusicWindow(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Music')
+
+        layout = QtWidgets.QGridLayout()
+
+        self.button = QtWidgets.QPushButton('Music')
+        self.button.clicked.connect(self.music)
+
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def music(self):
+        self.switch_window.emit()
+
+
+class Controller:
+
+    def __init__(self):
+        self.music = MusicWindow()
+
+    def show_music(self):
+        self.window.close()
+        self.music.switch_window.connect(self.show_main)
+        self.music.show()
+
+    def show_main(self):
+        self.music.close()
+        self.window = MainWindow()
+        self.window.switch_window.connect(self.show_music)
+        self.window.show()
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_main()
+    sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    ex = MainMenu()
-    sys.exit(app.exec_())
+    main()
+
+
